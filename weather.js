2acdade7091742920ae6df836cbe47f803de5a3b
G285262022 <228526@st.takushoku-u.ac.jp>
2025-07-17 16:41:31 +0900

    2

--- weather.js
+++ weather.js
@@ -58,17 +58,39 @@ function printDom(data) {
 
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
-
+document.getElementById('print').addEventListener('click', sendRequest);
 
 
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
+  const oldResults = document.querySelectorAll('#result');
+  oldResults.forEach(el => el.remove());
 
+  const checked = document.querySelectorAll('.map-container input[type="checkbox"]:checked');
+  const selectedIDs = Array.from(checked).map(cb => cb.value);
+
+  selectedIDs.forEach(id => {
+  const url = `https://www.nishita-lab.org/web-contents/jsons/openweather/${id}.json`;
+  fetch(url)
+      .then(response => response.json())
+      .then(showResult)
+      .catch(showError)
+      .finally(finish);
+  });
 }
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
+  let data;
+  if (typeof resp === "string") {
+    data = JSON.parse(resp);
+  } else {
+    data = resp;
+  }
+
+  print(data);     
+  printDom(data);  
 
 }
 
@@ -81,55 +103,4 @@ function showError(err) {
 function finish() {
     console.log('Ajax 通信が終わりました');
 }
-
-////////////////////////////////////////
-// 以下はグルメのデータサンプル
-// 注意: 第5回までは以下を変更しないこと！
-// 注意2: 課題6-1 で以下をすべて削除すること
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
 
